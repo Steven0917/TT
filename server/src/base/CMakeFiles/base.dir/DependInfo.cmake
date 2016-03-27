64f16dc11e222ca46170e571c656375b5dd95392
steven <tengxin1981@yahoo.com>
2016-03-28 00:05:31 +0800

    build on ubuntu 15.10

---
+++ server/src/base/CMakeFiles/base.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/Base64.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/Base64.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/BaseSocket.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/BaseSocket.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/Condition.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/Condition.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/ConfigFileReader.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/ConfigFileReader.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/EncDec.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/EncDec.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/EventDispatch.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/EventDispatch.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/HttpClient.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/HttpClient.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/HttpParserWrapper.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/HttpParserWrapper.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/ImPduBase.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/ImPduBase.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/Lock.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/Lock.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/ServInfo.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/ServInfo.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/Thread.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/Thread.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/ThreadPool.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/ThreadPool.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/TokenValidator.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/TokenValidator.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/UtilPdu.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/UtilPdu.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/http_parser.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/http_parser.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/im_conn_util.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/im_conn_util.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/imconn.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/imconn.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/jsoncpp/json_reader.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_reader.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/jsoncpp/json_value.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_value.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/jsoncpp/json_writer.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_writer.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/log4z.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/log4z.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/netlib.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/netlib.cpp.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.BaseDefine.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.BaseDefine.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.Buddy.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Buddy.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.File.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.File.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.Group.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Group.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.Login.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Login.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.Message.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Message.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.Other.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Other.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.Server.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Server.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/pb/protocol/IM.SwitchService.pb.cc" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.SwitchService.pb.cc.o"
+  "/home/tengx/workspc/TT/TeamTalk/server/src/base/util.cpp" "/home/tengx/workspc/TT/TeamTalk/server/src/base/CMakeFiles/base.dir/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "AC_HAS_CRITICAL"
+  "AC_HAS_DEBUG"
+  "AC_HAS_ERROR"
+  "AC_HAS_INFO"
+  "AC_HAS_WARNING"
+  "LINUX_DAEMON"
+  "TIXML_USE_STL"
+  "_FILE_OFFSET_BITS=64"
+  "_REENTRANT"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  ".."
+  "./jsoncpp"
+  "./pb"
+  "./pb/protocol"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
